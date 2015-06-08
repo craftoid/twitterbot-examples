2030b744650e3225f2363139cc98664074806342
Martin Schneider <craftoid@k2g2.org>
2015-06-08 11:06:47 +0200

    Improve comments

--- alicebot/alicebot.py
+++ alicebot/alicebot.py
@@ -7,12 +7,7 @@ import aiml
 
 class AliceBot(TwitterBot):
     def bot_init(self):
-        """
-        Initialize and configure your bot!
-
-        Use this function to set options and initialize your own custom bot
-        state (if any).
-        """
+        """ Initialize and configure alicebot """
 
         ############################
         # REQUIRED: LOGIN DETAILS! #
@@ -49,109 +44,44 @@ class AliceBot(TwitterBot):
         # follow back all followers?
         self.config['autofollow'] = False
 
-
-        ###########################################
-        # CUSTOM: your bot's own state variables! #
-        ###########################################
-        
-        # If you'd like to save variables with the bot's state, use the
-        # self.state dictionary. These will only be initialized if the bot is
-        # not loading a previous saved state.
-
-        # self.state['butt_counter'] = 0
-
-        # You can also add custom functions that run at regular intervals
-        # using self.register_custom_handler(function, interval).
-        #
-        # For instance, if your normal timeline tweet interval is every 30
-        # minutes, but you'd also like to post something different every 24
-        # hours, you would implement self.my_function and add the following
-        # line here:
-        
-        # self.register_custom_handler(self.my_function, 60 * 60 * 24)
-
-
+        # create a new chatterbot and load the AIML file
         self.alice = aiml.Kernel()
         self.alice.learn("german.aiml")
 
 
     def on_scheduled_tweet(self):
-        """
-        Make a public tweet to the bot's own timeline.
-
-        It's up to you to ensure that it's less than 140 characters.
-
-        Set tweet frequency in seconds with TWEET_INTERVAL in config.py.
-        """
-        # text = function_that_returns_a_string_goes_here()
-        # self.post_tweet(text)
-
-        pass 
+        """ Make a public tweet to the bot's own timeline. """
+        pass # don't do anything here ...
 
     def on_mention(self, tweet, prefix):
-        """
-        Defines actions to take when a mention is received.
-
-        tweet - a tweepy.Status object. You can access the text with
-        tweet.text
-
-        prefix - the @-mentions for this reply. No need to include this in the
-        reply string; it's provided so you can use it to make sure the value
-        you return is within the 140 character limit with this.
-
-        It's up to you to ensure that the prefix and tweet are less than 140
-        characters.
-
-        When calling post_tweet, you MUST include reply_to=tweet, or
-        Twitter won't count it as a reply.
-        """
-        # text = function_that_returns_a_string_goes_here()
-        # prefixed_text = prefix + ' ' + text
-        # self.post_tweet(prefix + ' ' + text, reply_to=tweet)
-
-        # call this to fav the tweet!
-        # if something:
-        #     self.favorite_tweet(tweet)
-
+        """ Actions to take when a mention is received. """
+  
         tweetsize = 140 - len(prefix) - 1
 
         try:
+
+            # get the question from the tweet
             question = tweet.text
+
+            # get a response from the chatterbot
             response = self.alice.respond(question)
+
+            # create a tweet and make sure to cut it off at 140 chars
             text = prefix + " " + response
             text = text[:140]
-            print(text)
+
+            # post the tweet
             self.post_tweet(text, reply_to=tweet)
 
         except Exception as e:
+        	
+        	# did anything go wrong when we tried to create and post the tweet?
             print(e)
 
 
     def on_timeline(self, tweet, prefix):
-        """
-        Defines actions to take on a timeline tweet.
-
-        tweet - a tweepy.Status object. You can access the text with
-        tweet.text
-
-        prefix - the @-mentions for this reply. No need to include this in the
-        reply string; it's provided so you can use it to make sure the value
-        you return is within the 140 character limit with this.
-
-        It's up to you to ensure that the prefix and tweet are less than 140
-        characters.
-
-        When calling post_tweet, you MUST include reply_to=tweet, or
-        Twitter won't count it as a reply.
-        """
-        # text = function_that_returns_a_string_goes_here()
-        # prefixed_text = prefix + ' ' + text
-        # self.post_tweet(prefix + ' ' + text, reply_to=tweet)
-
-        # call this to fav the tweet!
-        # if something:
-        #     self.favorite_tweet(tweet)
-        pass
+        """ Actions to take on a timeline tweet. """
+        pass # Don't do anything here ...
         
 
 if __name__ == '__main__':
